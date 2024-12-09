8d8324fdc0df72cc2996d8f3cf45848d90de32ab
zhouyu <2179853437@qq.com>
2024-12-09 14:13:14 +0800

    消息页面

--- app/src/main/java/edu/ace/infinite/fragment/MessageFragment.java
+++ app/src/main/java/edu/ace/infinite/fragment/MessageFragment.java
@@ -5,9 +5,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import edu.ace.infinite.R;
+import edu.ace.infinite.adapter.MessageAdapter;
+import edu.ace.infinite.model.Message;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MessageFragment extends BaseFragment{
+    private RecyclerView recyclerViewMessages;
+    private MessageAdapter messageAdapter;
+    private List<Message> messageList; // 假设有一个Message模型类
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -17,6 +29,18 @@ public class MessageFragment extends BaseFragment{
     }
 
     private void initViews() {
+        recyclerViewMessages = view.findViewById(R.id.recyclerView_messages);
+        recyclerViewMessages.setLayoutManager(new LinearLayoutManager(getContext()));
+        messageList = fetchMessages(); // 获取消息列表的方法
+        messageAdapter = new MessageAdapter(messageList);
+        recyclerViewMessages.setAdapter(messageAdapter);
+    }
 
+    private List<Message> fetchMessages() {
+        List<Message> messages = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            messages.add(new Message("消息1", "发送者1", ""));
+        }
+        return messages;
     }
 }
