4502d13754e3a28e01b1583206fdf24f59a2d971
zhouyu <2179853437@qq.com>
2024-12-09 21:56:36 +0800

    启动页

--- app/src/main/java/edu/ace/infinite/activity/StartActivity.java
+++ app/src/main/java/edu/ace/infinite/activity/StartActivity.java
@@ -5,6 +5,7 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.VideoView;
 
@@ -23,40 +24,60 @@ import java.util.Map;
 
 import edu.ace.infinite.R;
 import edu.ace.infinite.application.Application;
+import edu.ace.infinite.utils.ConsoleUtils;
 
 public class StartActivity extends BaseActivity {
+    private VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_start);
         super.onCreate(savedInstanceState);
 
-//        VideoView videoView = findViewById(R.id.videoView);
-//        ExoMediaPlayer exoMediaPlayer = new ExoMediaPlayer(view.getContext());
-//        exoMediaPlayer.setPlayWhenReady(true); // 自动播放
-//        videoView.setMediaPlayer(exoMediaPlayer);
-//
-//        ExoSourceBuilder exoSourceBuilder = new ExoSourceBuilder(this, );
-//        exoSourceBuilder.setCacheEnable(false);
-//        MediaSource exoMediaSource = exoSourceBuilder.build();
-//        exoMediaPlayer.setMediaSource();
+        videoView = findViewById(R.id.videoView);
 
-        VideoView videoView = findViewById(R.id.videoView);
+        // 设置视频 URI，使用 R 类中的资源 ID
         Uri videoUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.start_page_video);
         videoView.setVideoURI(videoUri);
-        videoView.setOnCompletionListener(MediaPlayer::start);
+
+        // 添加监听器
+        videoView.setOnPreparedListener(mp -> {
+            // 视频准备完成后自动播放
+            videoView.start();
+        });
+        videoView.setOnCompletionListener(mp -> {
+            // 当视频播放完毕时，重新开始播放
+            videoView.start();
+            skip();
+        });
+
+        // 开始准备视频
+        videoView.requestFocus();
 
 
-        new Handler().postDelayed(() -> {
-            Map<String, String> loginMessage = Application.getLoginMessage();
-            Intent intent;
-            String token = loginMessage.get("userToken");
-            if(token == null){
-                intent = new Intent(this, LoginActivity.class);
-            }else {
-                intent = new Intent(this, MainActivity.class);
+    }
+
+    private void skip() {
+        new Handler().post(new Runnable() {
+            @Override
+            public void run() {
+                Map<String, String> loginMessage = Application.getLoginMessage();
+                Intent intent;
+                String token = loginMessage.get("userToken");
+                if(token == null){
+                    intent = new Intent(StartActivity.this, LoginActivity.class);
+                }else {
+                    intent = new Intent(StartActivity.this, MainActivity.class);
+                }
+                startActivity(intent);
+                finish();
             }
-            startActivity(intent);
-        },3000);
+        });
+    }
+
+    @Override
+    public void finish() {
+        videoView.resume();
+        super.finish();
     }
 }
