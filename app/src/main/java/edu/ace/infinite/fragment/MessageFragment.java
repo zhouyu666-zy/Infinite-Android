164be6bbe47671fa9926a39391d63d154f15578a
xu341503 <2858737417@qq.com>
2024-12-09 20:08:43 +0800

    新增聊天功能及优化消息页面

--- app/src/main/java/edu/ace/infinite/fragment/MessageFragment.java
+++ app/src/main/java/edu/ace/infinite/fragment/MessageFragment.java
@@ -1,5 +1,6 @@
 package edu.ace.infinite.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,38 +10,65 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import edu.ace.infinite.R;
-import edu.ace.infinite.adapter.MessageAdapter;
-import edu.ace.infinite.model.Message;
+import edu.ace.infinite.activity.ChatActivity;
+import edu.ace.infinite.adapter.MessageListAdapter;
+import edu.ace.infinite.pojo.MessageListItem;
+
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MessageFragment extends BaseFragment{
-    private RecyclerView recyclerViewMessages;
-    private MessageAdapter messageAdapter;
-    private List<Message> messageList; // 假设有一个Message模型类
+public class MessageFragment extends BaseFragment {
+    private RecyclerView messageListRecyclerView;
+    private MessageListAdapter messageListAdapter;
+    private List<MessageListItem> messageList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+                           Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_message, container, false);
         initViews();
+        loadMessages();
         return view;
     }
 
     private void initViews() {
-        recyclerViewMessages = view.findViewById(R.id.recyclerView_messages);
-        recyclerViewMessages.setLayoutManager(new LinearLayoutManager(getContext()));
-        messageList = getMessages(); // 获取消息列表的方法
-        messageAdapter = new MessageAdapter(messageList);
-        recyclerViewMessages.setAdapter(messageAdapter);
+        messageListRecyclerView = view.findViewById(R.id.messageListRecyclerView);
+        messageListRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        
+        messageList = new ArrayList<>();
+        messageListAdapter = new MessageListAdapter(getContext(), messageList);
+        messageListRecyclerView.setAdapter(messageListAdapter);
+
+        messageListAdapter.setOnItemClickListener(item -> {
+            // 跳转到聊天界面
+            Intent intent = new Intent(getActivity(), ChatActivity.class);
+            intent.putExtra("userId", item.getUserId());
+            intent.putExtra("username", item.getUsername());
+            intent.putExtra("avatar", item.getAvatar());
+            startActivity(intent);
+        });
     }
 
-    private List<Message> getMessages() {
-        List<Message> messages = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            messages.add(new Message("消息1", "发送者1", ""));
-        }
-        return messages;
+    private void loadMessages() {
+        // TODO: 从服务器加载消息列表
+        // 这里先添加一些测试数据
+        messageList.add(new MessageListItem(
+            "1",
+            "一只小皇帝",
+            "",
+            "1小时内在线",
+            "31分钟前",
+            true
+        ));
+        messageList.add(new MessageListItem(
+            "2",
+            "幻想过爱",
+            "",
+            "在线",
+            "",
+            true
+        ));
+        messageListAdapter.notifyDataSetChanged();
     }
 }
